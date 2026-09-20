d830d938ecc6abb9a624998a6e96af999e717248
Madhavkris <madhavkrishm@gmail.com>
2026-09-20 20:56:40 +0530

    Implemented the update operation for the patients

--- routes/patient_route.py
+++ routes/patient_route.py
@@ -1,7 +1,7 @@
 from urllib import request
-
 from flask import Blueprint,jsonify,request
-from services.patient_service import get_all_patients,get_patient_by_id,create_patient
+from services.patient_service import get_all_patients,get_patient_by_id,create_patient,delete_patient,update_patient
+
 patient_bp=Blueprint('patient',__name__,url_prefix='/api/patients')
 @patient_bp.route('/',methods=['GET'])
 def get_patients():
@@ -55,3 +55,35 @@ def add_patient():
         "disease":created_patient.disease
 
     }),201
+
+@patient_bp.route('/<int:patient_id>',methods=['DELETE'])
+def remove_patient(patient_id):
+    patient=get_patient_by_id(patient_id)
+    if not patient:
+        return jsonify({"error":"Patient not found"}),404
+    target_id=patient_id
+    deleted_patient = delete_patient(patient_id)
+    if not deleted_patient:
+        return jsonify({"error":"Patient not deleted"}),400
+    return jsonify({"status":"Patient removed successfully","patient_id":target_id}),200
+
+
+@patient_bp.route('/<int:patient_id>',methods=['PUT'])
+def edit_patient(patient_id):
+    patient=request.get_json()
+    if not patient:
+        return jsonify({"error":"Request body is Required"}),400
+    updated_patient_name=patient.get('patient_name')
+    updated_age=patient.get('age')
+    updated_gender=patient.get('gender')
+    updated_disease=patient.get('disease')
+    updated_patient = update_patient(
+        patient_id=patient_id,
+        patient_name=updated_patient_name,
+        age=updated_age,
+        gender=updated_gender,
+        disease=updated_disease
+    )
+    if not updated_patient:
+        return jsonify({"error":"Patient not updated"}),400
+    return jsonify({"status":"Patient updated successfully","patient_id":updated_patient.patient_id}),200
--- services/patient_service.py
+++ services/patient_service.py
@@ -23,3 +23,29 @@ def create_patient(patient_nm,age,gender,disease):
         return None
 
 
+def delete_patient(patient_id):
+    try:
+       patient=get_patient_by_id(int(patient_id))
+       db.session.delete(patient)
+       db.session.commit()
+       return patient
+    except Exception as e:
+        db.session.rollback()
+        print(repr(e))
+        return None
+
+def update_patient(patient_id,patient_name,age,gender,disease):
+    try:
+        patient=get_patient_by_id(patient_id)
+        if not patient:
+            return None
+        patient.patient_name=patient_name
+        patient.age=age
+        patient.gender=gender
+        patient.disease=disease
+        db.session.commit()
+        return patient
+    except Exception as e:
+        db.session.rollback()
+        print(repr(e))
+        return None
